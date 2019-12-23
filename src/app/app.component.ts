76b8d40169839af37522600d3ebf2e650506ecc9
calpwin <calpwin@gmail.com>
2019-12-23 20:25:29 +0300

    add firebase + ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import * as firebase from "firebase";
+import * as firebaseui from "firebaseui";
+
 
 @Component({
   selector: 'cdc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'calpvin-devcenter';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    const firebaseConfig = {
+      apiKey: "AIzaSyA-muRhisD2v8EZuBYI_yaMLS8Fvx8h6qk",
+      authDomain: "calpvin-devcenter.firebaseapp.com",
+      databaseURL: "https://calpvin-devcenter.firebaseio.com",
+      projectId: "calpvin-devcenter",
+      storageBucket: "calpvin-devcenter.appspot.com",
+      messagingSenderId: "652397528928",
+      appId: "1:652397528928:web:0bbd9c773a9fad3b1e8005",
+      measurementId: "G-BXJYP0CSS6"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+
+      ui.start('#firebaseui-auth-container', {
+        signInOptions: [
+          {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+          }
+        ],
+        // Other config options...
+      });
+  }
 }
