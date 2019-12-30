b82f0eb0148147d25263bd230c0594dfaa83202b
calpwin <calpwin@gmail.com>
2019-12-30 18:26:40 +0300

    /dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,24 @@ import {Component, OnInit} from '@angular/core';
 import * as BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
 import {CKEditor5} from "@ckeditor/ckeditor5-angular";
 
+export class MyUploadAdapter {
+  constructor(public loader) {
+  }
+
+  upload() {
+    return this.loader.file.then(file => new Promise(( resolve, reject ) =>{
+      file.arrayBuffer().then(b =>
+      {
+        console.log(b);
+      });
+    }))
+  }
+
+  abort() {
+
+  }
+}
+
 @Component({
   selector: 'cdc-root',
   templateUrl: './app.component.html',
@@ -13,14 +31,12 @@ export class AppComponent implements OnInit {
   public Editor = BalloonEditor;
 
   public Config = {
-    ckfinder: {
-      // Upload the images to the server using the CKFinder QuickUpload command.
-      uploadUrl: '/'
-    }
   };
 
   public onEditorReady(editor: CKEditor5.Editor) {
-    console.log(editor.plugins._availablePlugins.get('CKFinder').pluginName);
+    editor.plugins.get('FileRepository').createUploadAdapter = ( loader ) => {
+      return new MyUploadAdapter( loader );
+    };
   }
 
   constructor() {
